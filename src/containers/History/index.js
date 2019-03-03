63dac8a5ce0d7acf8bdfbc13710de8bdf435bf9b
Dariusz Sibik <sibikdariusz@gmail.com>
2019-03-03 01:02:05 +0100

    add Redux Provider

--- src/containers/History/index.js
+++ src/containers/History/index.js
@@ -1,17 +1,16 @@
-
 import { connect } from 'react-redux';
 import HistoryView from '../../components/History';
-import { updateHistory } from '../../actions/history';
 import { toggleHistory } from '../../actions/history';
+import { updateExpression } from '../../actions/expression';
 
 export const historyItemClickHandler = dispatch => history => {
-    dispatch(updateHistory(history));
-    dispatch(toggleHistory());
+  dispatch(updateExpression(history));
+  dispatch(toggleHistory());
 }
 
 const mapDispatchToProps = (dispatch) => ({
-    historyItemClickHandler: historyItemClickHandler(dispatch),
-    toggleHistory: () => dispatch(toggleHistory())
+  historyItemClickHandler: historyItemClickHandler(dispatch),
+  toggleHistory: () => dispatch(toggleHistory())
 })
 
 export default connect(null, mapDispatchToProps)(HistoryView);
